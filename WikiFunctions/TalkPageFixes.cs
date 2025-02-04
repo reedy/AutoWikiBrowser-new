16a3d75d27676a4dd38d860cbd8cc9af2641cb49
Rob Wilmshurst <rjwilmsi@users.sourceforge.net>
2025-02-04 20:15:17 +0000

    T270920 Update EnglishVariationsTemplates
    git-svn-id: svn://svn.code.sf.net/p/autowikibrowser/code/AWB@12840 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/TalkPageFixes.cs
+++ WikiFunctions/TalkPageFixes.cs
@@ -237,7 +237,15 @@ namespace WikiFunctions.TalkPages
         private static readonly Regex GANomineeTemplate = Tools.NestedTemplateRegex(new [] { "GA nominee", "GAnominee", "GA" });
         private static readonly Regex TalkWarningTemplates = Tools.NestedTemplateRegex(new[] { "Community article probation", "Censor", "Controversial", "BLP others", "COI editnotice", "Notice", "warning", "Austrian economics sanctions" });
         private static readonly Regex TalkGuidelineTemplates = Tools.NestedTemplateRegex(new[] { "Not a forum", "Recurring themes", "FAQ", "Round in circles", "Calm", "Pbneutral" });
-        private static readonly Regex EnglishVariationsTemplates = Tools.NestedTemplateRegex(new[] { "American English", "Australian English", "British English", "British English Oxford spelling", "Canadian English", "Hiberno-English", "Indian English", "Malaysian English", "Malawian English", "New Zealand English", "Pakistani English", "Philippine English", "Scottish English", "South African English", "Trinidadian English" });
+
+        private static readonly Regex EnglishVariationsTemplates = Tools.NestedTemplateRegex(new[]
+        {
+            "American English editnotice", "Antiguan and Barbudan English", "Australian English editnotice",
+            "Bangladeshi English", "British English editnotice", "British English Oxford spelling editnotice",
+            "Canadian English editnotice", "Ghanaian English", "Hong Kong English", "IUPAC spelling",
+            "IUPAC spelling US", "Jamaican English", "Kenyan English", "Liberian English", "Nigerian English",
+            "Sierra Leonean English", "Singaporean English", "Sri Lankan English", "Ugandan English"
+        });
         private static readonly Regex TalkHistoryTemplates = Tools.NestedTemplateRegex(new[] { "Article history", "ArticleHistory" });
         private static readonly Regex TalkHistoryBTemplates = Tools.NestedTemplateRegex(new[] { "FailedGA", "Old prod", "Old prod full", "Oldprodfull", "Afd-merged-from", "Old AfD multi", "Old AfD", "Oldafdfull ", "Old peer review", "Old CfD", "Old RfD", "Old XfD multi", "Old XfD" });
         private static readonly Regex MilestoneTemplates = Tools.NestedTemplateRegex(new[] { "DYK talk", "ITN talk", "On this day" });
