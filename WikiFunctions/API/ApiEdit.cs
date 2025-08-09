b28281d31067dac18ecf8fb42cf5316d41749e0a
David Brooks <davidbrooks1949@users.sourceforge.net>
2025-08-09 22:02:47 +0000

    T399860 handle EmailAuth on login - extend to any server with the extension
    git-svn-id: svn://svn.code.sf.net/p/autowikibrowser/code/AWB@13002 5227b50d-9349-4574-b055-4b0a7525e11c

--- WikiFunctions/API/ApiEdit.cs
+++ WikiFunctions/API/ApiEdit.cs
@@ -611,8 +611,7 @@ namespace WikiFunctions.API
             string token = xr.GetAttribute("logintoken");
 
             // If not a bot, use the clientlogin API, which gives an opportunity to supply a OTC
-            // For now, the EmailAuth method is only handled on the en wikipedia.
-            if (!username.Contains("@") && !string.IsNullOrEmpty(token) && Variables.LangCode == "en")
+            if (!username.Contains("@") && !string.IsNullOrEmpty(token))
             {
                 ClientLogin(username, password, token);
             }
@@ -693,6 +692,12 @@ namespace WikiFunctions.API
 
         public void ClientLogin(string username, string password, string token)
         {
+            Dictionary<string, string> postparams = new Dictionary<string, string>
+            {
+                {"password", password},
+                {"logintoken", token}
+            };
+
             string result = HttpPost(
                 new Dictionary<string, string>
                 {
@@ -700,11 +705,7 @@ namespace WikiFunctions.API
                     {"username", username},
                     {"loginreturnurl", "https://en.wikipedia.org/"} // Not used but required by API
                 },
-                new Dictionary<string, string>
-                {
-                    {"password", password},
-                    {"logintoken", token}
-                }
+                postparams
             );
 
             XmlReader xr = CreateXmlReader(result);
@@ -715,55 +716,69 @@ namespace WikiFunctions.API
             string status = (xr.GetAttribute("status") ?? "").ToUpper();
             if (status == "PASS")
                 return;
-            string message = xr.GetAttribute("message");
-            if (status != "UI" || message == null ||
-                message.IndexOf("A verification code has been sent", StringComparison.InvariantCultureIgnoreCase) < 0)
+            string message = xr.GetAttribute("message") ?? "";
+            // Makes the (unverified) assumption that the email will be in parens in all localizations
+            Match emailMatch = Regex.Match(message, @"\(.+?@.+?\)");
+            if (status != "UI" || !emailMatch.Success)
             {
                 throw new LoginException(this, status);
             }
 
-            // The message is too long for InputBox. Assume the current syntax.
-            Match m = Regex.Match(message, @"\(.*?\)");
-            if (m.Success)
-            {
-                message = "Enter the code sent to your email " + m.Value;
-            }
-            InputBoxResult coderesult = InputBox.Show(message, "Enter One-Time-Code", "", ClientLoginValidator);
-            if (!coderesult.OK)
-            {
-                throw new LoginException(this, "Login cancelled");
-            }
-
-            // As of July 2025 EN uses EmailAuth, so that's the most likely.
-            // OATH and WebAuthn may need to be implemented for other wikis though.
+            // Handle 2FA. For now, only EmailAuth is supported (until we can test OATHAuth).
+            postparams.Clear();
             result = HttpPost(
                 new Dictionary<string, string>
                 {
-                    {"action", "clientlogin"},
-                    {"logincontinue", "1"},
-                    {"token", coderesult.Text},
+                    {"action", "query"},
+                    {"meta", "siteinfo"},
+                    {"siprop", "extensions"}
                 },
-                new Dictionary<string, string>
-                {
-                    {"logintoken", token},
-                }
+                postparams
             );
 
             xr = CreateXmlReader(result);
-
-            Tools.WriteDebug("API::Edit action/clientlogin2", result);
-
-            xr.ReadToFollowing("clientlogin");
-            status = (xr.GetAttribute("status") ?? "").ToUpper();
-            if (status == "PASS")
+            while (xr.ReadToFollowing("ext"))
             {
-                return;
+                if (xr.GetAttribute("name") == "EmailAuth")
+                    break;
+            }
+            if (!xr.EOF)
+            {
+                // The message itself is too long for InputBox. Continue to assume the (email) syntax.
+                InputBoxResult coderesult = InputBox.Show(
+                    "Enter the code sent to your email " + emailMatch.Value + '.',
+                    "Enter One-Time-Code", "", ClientLoginValidator);
+                if (!coderesult.OK)
+                {
+                    throw new LoginException(this, "Login cancelled");
+                }
+
+                postparams.Add("logintoken", token);
+                result = HttpPost(
+                    new Dictionary<string, string>
+                    {
+                        {"action", "clientlogin"},
+                        {"logincontinue", "1"},
+                        {"token", coderesult.Text},
+                    },
+                    postparams
+                );
+
+                xr = CreateXmlReader(result);
+
+                Tools.WriteDebug("API::Edit action/clientlogin2", result);
+
+                xr.ReadToFollowing("clientlogin");
+                status = (xr.GetAttribute("status") ?? "").ToUpper();
+                if (status == "PASS")
+                    return;
+
+                // If status is UI (user entered the wrong code) we could loop back and try again,
+                // but there's a danger of getting caught in a loop.
+                // Also, OATHAuth should work the same way, using the OATHToken parameter, but that's not tested.
             }
 
-            // At this point, if status is UI (user entered the wrong code) we could loop back and try again,
-            // but there's a danger of getting caught in a loop.
-
-            throw new LoginException(this, status);
+            throw new LoginException(this, "Verification failed - " + status);
         }
 
         private static void ClientLoginValidator(object sender, InputBoxValidatingArgs e)
